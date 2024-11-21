608d3f11950403329262fcd5e46759acef5a7686
lee0802-120 <lpk3782@gmail.com>
2024-11-21 17:49:53 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@ import DiaryComponent from "./components/DiaryComponent";
 import FindPw from "./pages/user/FindPw";
 import Join from "./pages/user/Join";
 import Login from "./pages/user/Login";
-<<<<<<< HEAD
 import Chat from "./layout/chat/chat";
-=======
-import Logout from "./pages/user/Logout";
->>>>>>> 001285b29daacfb596d81aa48400d66a32c7168a
 // import FileTest from "./pages/file/FileTest";
 
 function App() {
   return (
-<<<<<<< HEAD
     <>
       <Routes>
         {/* <Route path="/upload" element={<FileTest />} /> */}
@@ -24,16 +19,6 @@ function App() {
       </Routes>
       <Chat />
     </>
-=======
-    <Routes>
-      {/* <Route path="/upload" element={<FileTest />} /> */}
-      <Route path="/" element={<DiaryComponent />} />
-      <Route path="/user/FindPw" element={<FindPw />} />
-      <Route path="/user/Join" element={<Join />} />
-      <Route path="/user/login" element={<Login />} />
-      <Route path="/user/Logout" element={<Logout />} />
-    </Routes>
->>>>>>> 001285b29daacfb596d81aa48400d66a32c7168a
   );
 }
 export default App;
