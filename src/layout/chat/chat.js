c109b3ce23e4c98eed1c62411c28175300b9e84e
lee0802-120 <lpk3782@gmail.com>
2024-11-22 16:28:10 +0900

    채팅 숨김 기능.

--- src/layout/chat/chat.js
+++ src/layout/chat/chat.js
@@ -7,27 +7,50 @@ import {
   Navbar,
   Avatar,
 } from "react-chat-elements";
-import { Route, Routes } from "react-router-dom";
-import { SlActionRedo } from 'react-icons/sl';
-
-/* 
-
-    {
-      position:"left",
-      type:"text",
-      title:"Kursat",
-      text:"Give me a message list example !",
-    },
-    {
-      position:"right",
-      type:"text",
-      title:"Emre",
-      text:"That's all.",
-    },
-
-*/
+import { Route } from "react-router-dom";
+import { SlActionRedo } from "react-icons/sl";
+import { IoMdClose } from 'react-icons/io';
+import React, { useState } from "react";
 
 const Chat = () => {
+  const [chatList, setChatList] = useState([
+    {
+      position: "left",
+      type: "text",
+      title: "Kursat",
+      text: "Give me a message list example !",
+      date: new Date(),
+    },
+    {
+      position: "right",
+      type: "text",
+      title: "Emre",
+      text: "That's all.",
+      date: new Date(),
+    },
+  ]);
+  const [newMessage, setNewMessage] = useState("");
+  let inputClear = () => {};
+  const inputReferance = React.useRef();
+
+  const sendMessage = () => {
+    if (!newMessage.trim()) return;
+
+    setChatList((prevChatList) => [
+      ...prevChatList,
+      {
+        position: "right",
+        type: "text",
+        title: "Emre",
+        text: newMessage,
+        date: new Date(),
+      },
+    ]);
+
+    inputClear();
+    setNewMessage("");
+  };
+
   return (
     <div className="chat_container">
       <Navbar
@@ -41,38 +64,43 @@ const Chat = () => {
           />
         }
         center={<p>center</p>}
-        right={<p></p>}
+        right={
+          <Button
+            type='transparent'
+            color='rgba(0, 0, 0, 1)'
+            icon={{
+              float: 'right',
+              size: 20,
+              component: <IoMdClose />
+            }}
+          />
+        }
       />
       <MessageList
         className="chat-message-list"
         lockable={true}
         toBottomHeight={"100%"}
-        dataSource={[
-          {
-            position: "left",
-            type: "text",
-            title: "Kursat",
-            text: "Give me a message list example !",
-          },
-          {
-            position: "right",
-            type: "text",
-            title: "Emre",
-            text: "That's all.",
-          },
-        ]}
+        dataSource={chatList}
       />
       <div className="chat-message-div-input">
         <Input
           className="chat-message-input"
           placeholder="Type here..."
-          rightButtons={<Button title="send" icon={
-            {
-              float: "left",
-              size: 12,
-              component: <SlActionRedo/>
-            }
-          } />}
+          referance={inputReferance}
+          clear={(clear) => inputClear = clear}
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          rightButtons={
+            <Button
+              title="send"
+              icon={{
+                float: "left",
+                size: 15,
+                component: <SlActionRedo />,
+              }}
+              onClick={sendMessage}
+            />
+          }
         />
       </div>
     </div>
