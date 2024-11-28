6785abe609713ad44f5a215d1bc67ef5538f14f0
OriNong <min000922@gmail.com>
2024-11-28 17:47:13 +0900

    일기장 레이아웃 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import DiaryComponent from "./components/DiaryComponent";
+import DiaryWritePage from "./pages/diary/DiaryWritePage";
 import FindPw from "./pages/user/FindPw";
 import Join from "./pages/user/Join";
 import Login from "./pages/user/Login";
@@ -36,7 +36,7 @@ function App() {
     <>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<DiaryComponent />} />
+          <Route path="/" element={<DiaryWritePage />} />
           <Route path="/user/FindPw" element={<FindPw />} />
           <Route path="/user/Join" element={<Join />} />
           <Route path="/user/login" element={<Login />} />
